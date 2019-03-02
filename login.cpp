568124eb22221b1e45e43ed5a704e5c3d67591ca
HwDxBIMA <bnagantara@gmail.com>
2019-03-02 19:55:58 +0700

    program login cpp

---
+++ login.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string username,password;
+	cout << "	Program Login Sederhana		"<<endl;
+	cout << "Masukan Username : ";cin >>username;
+	cout << "Masukan Password : ";cin >>password;
+	
+	if(username=="admin"&&password=="admin123")
+	cout << "anda berhasil login...";
+	else 
+	cout << "login gagal, username dan password salah";
+	return 0;
+}
